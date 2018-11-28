f9c609065523607bafd334f06893b8a879f5307c
vmai <vmai@inovex.de>
2018-11-28 21:53:09 +0100

    set up project

---
+++ TheUltimateTree/cmake-build-debug/CMakeFiles/TheUltimateTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheUltimateTree.dir/main.cpp.o"
+  "TheUltimateTree.pdb"
+  "TheUltimateTree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheUltimateTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
