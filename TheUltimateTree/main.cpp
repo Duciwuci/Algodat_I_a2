3512438164bd16aad8870536eadea1ed55cfa7ad
Reawel <32129483+Reawel@users.noreply.github.com>
2019-01-15 00:18:58 +0100

    Update main.cpp

--- TheUltimateTree/main.cpp
+++ TheUltimateTree/main.cpp
@@ -5,27 +5,33 @@
 using namespace std;
 int main() {
     Trie<string> tree = Trie<string>();
-    pair<string, string> toAdd = pair<string, string>("abcd", "Welt");
+    pair<string, string> toAdd = pair<string, string>("Welt", "World");
 
-    pair<string, string> toAdd2 = pair<string, string>("abcdf", "Welt");
+    pair<string, string> toAdd2 = pair<string, string>("Wel", "Wooorld");
 
     tree.insert(toAdd);
+    tree.insert(toAdd2);
+
+    auto bla = tree.find("Welt");
+    auto blub = tree.find("Wel");
+
+    tree.insert(toAdd2);
+
+    std::cout << bla.operator*() << std::endl;
+    std::cout << blub.operator*() << std::endl;
+
+
     /*
     std::cout << tree.empty() << std::endl;
     tree.erase("abcd");
     std::cout << tree.empty() << std::endl;
     tree.insert(toAdd);
     tree.insert(toAdd2);
-
     cout << "find " << tree.find("abcdf").getValue() << endl;
-
-
     tree.erase("abcdf");
-
     std::cout << tree.empty() << std::endl;
     tree.erase("abcd");
     std::cout << tree.empty() << std::endl;
-
 */
     return 0;
 }
