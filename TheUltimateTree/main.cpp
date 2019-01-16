8c6cd422883b5afca4e3267af605a14f1d48ad52
Reawel <32129483+Reawel@users.noreply.github.com>
2019-01-16 22:46:23 +0100

    Update main.cpp

--- TheUltimateTree/main.cpp
+++ TheUltimateTree/main.cpp
@@ -5,20 +5,51 @@
 using namespace std;
 int main() {
     Trie<string> tree = Trie<string>();
-    pair<string, string> toAdd = pair<string, string>("Welt", "World");
+    pair<string, string> toAdd = pair<string, string>("sind", "are");
 
-    pair<string, string> toAdd2 = pair<string, string>("Wel", "Wooorld");
+    pair<string, string> toAdd2 = pair<string, string>("weis", "know");
+    pair<string, string> toAdd3 = pair<string, string>("wer", "who");
+    pair<string, string> toAdd4 = pair<string, string>("wir", "we");
+    pair<string, string> toAdd5 = pair<string, string>("wird", "will");
+    pair<string, string> toAdd6 = pair<string, string>("wo", "where");
 
     tree.insert(toAdd);
     tree.insert(toAdd2);
+    tree.insert(toAdd3);
+    tree.insert(toAdd4);
+    tree.insert(toAdd5);
+    tree.insert(toAdd6);
 
-    auto bla = tree.find("Welt");
-    auto blub = tree.find("Wel");
+    auto it = tree.begin();
+    std::cout << it.operator*() << std::endl;
+    it.operator++();
+    it.operator--();
+    std::cout << it.operator*() << std::endl;
+    it.operator++();
+    std::cout << it.operator*() << std::endl;
+    it.operator++();
+    std::cout << it.operator*() << std::endl;
+    it.operator++();
+    std::cout << it.operator*() << std::endl;
+    it.operator++();
+    std::cout << it.operator*() << std::endl;
 
-    tree.insert(toAdd2);
+
+
+    auto bla = tree.find("sind");
+    //auto blub = tree.find("Wird");
 
     std::cout << bla.operator*() << std::endl;
-    std::cout << blub.operator*() << std::endl;
+    bla = tree.find("weis");
+    std::cout << bla.operator*() << std::endl;
+    bla = tree.find("wer");
+    std::cout << bla.operator*() << std::endl;
+    bla = tree.find("wir");
+    std::cout << bla.operator*() << std::endl;
+    bla = tree.find("wird");
+    std::cout << bla.operator*() << std::endl;
+    bla = tree.find("wo");
+    std::cout << bla.operator*() << std::endl;
 
 
     /*
