2385373d3c0a844bd28c62cf9e1d871236756434
Reawel <32129483+Reawel@users.noreply.github.com>
2019-01-18 16:24:23 +0100

    added menu points for adding and searching words

--- TheUltimateTree/main.cpp
+++ TheUltimateTree/main.cpp
@@ -2,6 +2,14 @@
 #include "Trie.h"
 #include <string>
 
+void addWord(Trie<string> * tree);
+
+void findWord(Trie<string> * tree);
+
+void iterateAll(Trie<string> * tree);
+
+void listMenu();
+
 using namespace std;
 int main() {
     Trie<string> tree;
@@ -12,55 +20,76 @@ int main() {
     pair<string, string> toAdd4 = pair<string, string>("wir", "we");
     pair<string, string> toAdd5 = pair<string, string>("wird", "will");
     pair<string, string> toAdd6 = pair<string, string>("wo", "where");
+    pair<string, string> toAdd7 = pair<string, string>("test","test");
+    pair<string, string> toAdd8 = pair<string, string>("hallo","hello");
+    pair<string, string> toAdd9 = pair<string, string>("schoen","nice");
 
-    tree.insert(toAdd);
     tree.insert(toAdd2);
     tree.insert(toAdd3);
+    tree.insert(toAdd);
+    tree.insert(toAdd6);
     tree.insert(toAdd4);
     tree.insert(toAdd5);
-    /*tree.insert(toAdd6);
+    tree.insert(toAdd8);
+    tree.insert(toAdd9);
+    tree.insert(toAdd7);
+    std::string answer = "";
+    while (answer != "5") {
+        std::cout << "Was wollen Sie tun? (Menüpunkte --> 0)" << std::endl;
 
-    */auto it = tree.begin();
-    std::cout << *it << std::endl;
-    ++it;
-    --it;
-    std::cout << *it << std::endl;
-    ++it;
-    std::cout << *it << std::endl;
-    ++it;
-    std::cout << it.operator*() << std::endl;
-    /*it.operator++();
-    std::cout << it.operator*() << std::endl;
-    it.operator++();
-    std::cout << it.operator*() << std::endl;
+        std::cin >> answer;
 
-
-    auto bla = tree.find("sind");
-    //auto blub = tree.find("Wird");
-
-    std::cout << bla.operator*() << std::endl;
-    bla = tree.find("weis");
-    std::cout << bla.operator*() << std::endl;
-    bla = tree.find("wer");
-    std::cout << bla.operator*() << std::endl;
-    bla = tree.find("wir");
-    std::cout << bla.operator*() << std::endl;
-    bla = tree.find("wird");
-    std::cout << bla.operator*() << std::endl;
-    bla = tree.find("wo");
-    std::cout << bla.operator*() << std::endl;
- */
-    /*
-    std::cout << tree.empty() << std::endl;
-    tree.erase("abcd");
-    std::cout << tree.empty() << std::endl;
-    tree.insert(toAdd);
-    tree.insert(toAdd2);
-    cout << "find " << tree.find("abcdf").getValue() << endl;
-    tree.erase("abcdf");
-    std::cout << tree.empty() << std::endl;
-    tree.erase("abcd");
-    std::cout << tree.empty() << std::endl;
-*/
+        switch (std::stoi(answer)) {
+            case 0: listMenu();
+                break;
+            case 1: addWord(&tree);
+                break;
+            case 2: findWord(&tree);
+                break;
+            case 5: break;
+            default: std::cout << answer << " ist kein Menüpunkt!";
+        }
+    }
     return 0;
+}
+
+void addWord(Trie<string> * tree) {
+    string key;
+    string word;
+
+    std::cout << "Eingabe des deutschen Worts" << std::endl;
+    std::cin >> key;
+    std::cout << "Übersetzung" << std::endl;
+    std::cin >> word;
+
+    pair<string, string> element = pair<string, string>(key, word);
+    tree->insert(element);
+    std::cout << key << " mit Übersetzung: " << word << " wurde hinzugefügt." << std::endl;
+}
+
+void findWord(Trie<string> * tree) {
+    string key;
+
+    std::cout << "Welches Wort soll übersetzt werden?" << std::endl;
+    std::cin >> key;
+    auto it = tree->find(key);
+    if (it == tree->end()) {
+        std::cout << "Es tut mir leid, aber ich kann für " << key << " leider keine Übersetzung finden." << std::endl;
+    } else {
+        std::cout << key << " übersetzt bedeutet: " << *it << std::endl;
+    }
+}
+
+void iterateAll(Trie<string> * tree) {
+    for (auto i = tree->begin(); i != tree->end(); ++i) {
+        std::cout << *i << std::endl;
+    }
+}
+
+void listMenu() {
+    std::cout << "Menü:" << std::endl;
+    std::cout << " 0: Menü" << std::endl;
+    std::cout << " 1: Wort hinzufügen" << std::endl;
+    std::cout << " 2: Suchen einer Übersetzung" << std::endl;
+    std::cout << " 5: Beenden" << std::endl;
 }
